3d8f0dd81318544d8acb1bda062d08854a964a78
Ross Coker <rcoker37@gmail.com>
2018-03-20 12:49:23 -0400

    Added resources and started second level

---
+++ Assets/CameraDownTrigger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDownTrigger : MonoBehaviour {
+    public readonly Camera cam;
+
+    public readonly int camSizeAbove;
+    public readonly float camYPositionAbove;
+
+    public readonly int camSizeBelow;
+    public readonly float camYPositionBelow;
+
+    private bool enteredFromAbove;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            enteredFromAbove = IsPlayerAbove(collision.collider.gameObject);
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            bool exitingFromAbove = IsPlayerAbove(collision.collider.gameObject);
+
+            if (exitingFromAbove && !enteredFromAbove)
+            {
+                cam.transform.position = new Vector3(cam.transform.position.x, camYPositionAbove, cam.transform.position.z);
+                cam.orthographicSize = camSizeAbove;
+            } else if (!exitingFromAbove && enteredFromAbove)
+            {
+                cam.transform.position = new Vector3(cam.transform.position.x, camYPositionBelow, cam.transform.position.z);    
+                cam.orthographicSize = camSizeBelow;
+            }
+        }
+    }
+
+    public bool IsPlayerAbove(GameObject player)
+    {
+        return player.transform.position.y > transform.position.y;
+    }
+}
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -13,11 +13,11 @@ public class Obstacle : MonoBehaviour {
 	    	
 	}
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
-            collision.collider.gameObject.GetComponent<PlayableCharacter>().Kill();
+            //collision.collider.gameObject.GetComponent<PlayableCharacter>().Kill();
         }
 
         Destroy(gameObject);
