f15d8c3894b566a652293fd42f16ed69a49049f0
Ross Coker <rcoker37@gmail.com>
2018-03-26 12:15:17 -0400

    Added slime arrow animation

--- Assets/Scripts/SlimeGuide.cs
+++ Assets/Scripts/SlimeGuide.cs
@@ -6,21 +6,48 @@ public class SlimeGuide : MonoBehaviour {
 
     public GameObject portal;
     public GameObject player;
+    public Sprite[] sprites;
+
     public float distance = 2.0f;
-    public float angleForPosition;
-    public float angleForRotation;
+
+    private SpriteRenderer sr;
+    private int spriteIndex;
+    private int spriteStaticFrames = 5;
+
+    private IEnumerator animCoroutine;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        spriteIndex = 0;
+        sr.sprite = sprites[spriteIndex];
+        animCoroutine = Animate();
+        StartCoroutine(animCoroutine);    
+    }
+
+    private IEnumerator Animate()
+    {
+        while (spriteIndex < 5 + spriteStaticFrames)
+        {
+            spriteIndex++;
+            sr.sprite = spriteIndex < spriteStaticFrames ? sprites[0] : sprites[spriteIndex - spriteStaticFrames];
+            yield return new WaitForSecondsRealtime(0.16f);
+        }
+    }
+
+     
+
+    // Update is called once per frame
+    void Update () {
         Vector2 playerToPortal = portal.transform.position - player.transform.position;
 
-        angleForRotation = Vector2.SignedAngle(Vector2.right, playerToPortal) - 90.0f;
-        angleForPosition = Vector2.SignedAngle(Vector2.right, playerToPortal);
+        float angleForRotation = Vector2.SignedAngle(Vector2.right, playerToPortal) - 90.0f;
+        float angleForPosition = Vector2.SignedAngle(Vector2.right, playerToPortal);
         float angleForPositionRad = angleForPosition * Mathf.PI / 180.0f;
 
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, angleForRotation));
