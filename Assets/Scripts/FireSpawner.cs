2a2a8b0d95a4a68da1c943e4ba55dc3d08ba7ba8
Ross Coker <rcoker37@gmail.com>
2018-03-29 00:08:48 -0400

    Charred level done

---
+++ Assets/Scripts/FireSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpawner : MonoBehaviour {
+
+    public float SPAWN_RATE;
+    public float SPAWN_OFFSET;
+    public float LAUNCH_VELOCITY;
+    public float Y_OFFSET;
+    public GameObject firePrefab;
+
+    private float timeToSpawn;
+
+	void Start () {
+        timeToSpawn = SPAWN_OFFSET;
+	}
+
+    void FixedUpdate () {
+        if (timeToSpawn <= 0.0f)
+        {
+            Spawn();
+            timeToSpawn = 1.0f / SPAWN_RATE;
+        }
+
+        timeToSpawn -= Time.fixedDeltaTime;
+	}
+
+    private void Spawn()
+    {
+        GameObject fire = Instantiate(firePrefab);
+        fire.transform.position = new Vector3(transform.position.x, transform.position.y + Y_OFFSET, transform.position.z);
+        fire.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, LAUNCH_VELOCITY);
+    }
+}
---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+
+    // Use this for initialization
+    private void Update()
+    {
+        if (GetComponent<Rigidbody2D>().velocity.y >= 0)
+        {
+            GetComponent<SpriteRenderer>().flipY = true;
+        } else
+        {
+            GetComponent<SpriteRenderer>().flipY = false;
+        }
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.CompareTag("Player"))
+		{
+			collision.collider.gameObject.GetComponent<PlayerDemo>().Kill();
+		}
+
+		if (!collision.collider.CompareTag("Obstacle"))
+		{
+			GetComponent<BoxCollider2D>().enabled = false;
+			GetComponent<SpriteRenderer>().enabled = false;
+            Destroy(gameObject);
+		}
+	}
+}
